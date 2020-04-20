9908c13ef45cc86e49c9d2acebe91c6e93bb1c26
AbrahamAbiy <56045140+AbrahamAbiy@users.noreply.github.com>
2020-04-20 18:50:03 +0100

    Update GetFromApi.py

--- GetFromApi.py
+++ GetFromApi.py
@@ -2,15 +2,15 @@ from flask import Flask, render_template, request, jsonify
 import json
 import requests
 import requests_cache
-requests_cache.install_cache('crime_api_cache', backend='sqlite', expire_after=36$
+requests_cache.install_cache('crime_api_cache', backend='sqlite', expire_after=3600)
 app = Flask(__name__)
-street_url_template = 'https://data.police.uk/api/stops-street?lat={lat}&lng={lng$
+street_url_template = 'https://data.police.uk/api/stops-street?lat={lat}&lng={lng}&date={date}' #collecting data from this api
 @app.route('/stops-street', methods=['GET'])
 def stopchart():
         my_latitude = request.args.get('lat','51.52369')
         my_longitude = request.args.get('lng','-0.0395857')
         my_date = request.args.get('date','2017-12')
-        crime_url = street_url_template.format(lat = my_latitude, lng = my_longit$
+        crime_url = street_url_template.format(lat = my_latitude, lng = my_longitude, date = my_date) #specify params of get
         resp = requests.get(crime_url)
         if resp.ok:
                 return jsonify(resp.json())
